efcb85c468a5e70769d65e3b7c398f507d5a438f
Mats Dale <14minerboe16@gmail.com>
2017-10-19 12:35:32 -0600

    stuff

--- 2.rb
+++ 2.rb
@@ -1,4 +1,5 @@
 require './car'
+require './trip'
 
 corolla	   	  = Car.new("Toyota", "Corolla", 40, 13.2) 
 civic		  = Car.new("Honda", "Civic", 42, 12.4)
@@ -13,4 +14,20 @@ trip3 = Trip.new("San Francisco", "Los Angeles", 382)
 trip4 = Trip.new("Los Angeles", "Phoenix", 310)
 trip5 = Trip.new("Phoenix", "Denver", 821)
 
-trip1.totalCostOfCarTrip(corolla)
+corollaTripCost = trip1.totalCost(corolla) + trip2.totalCost(corolla) + trip3.totalCost(corolla) + trip4.totalCost(corolla) + trip5.totalCost(corolla)
+puts corollaTripCost
+
+civicTripCost = trip1.totalCost(civic) + trip2.totalCost(civic) + trip3.totalCost(civic) + trip4.totalCost(civic) + trip5.totalCost(civic)
+puts civicTripCost
+
+mustangTripCost = trip1.totalCost(mustang) + trip2.totalCost(mustang) + trip3.totalCost(mustang) + trip4.totalCost(mustang) + trip5.totalCost(mustang)
+puts mustangTripCost
+
+rogueTripCost = trip1.totalCost(rogue) + trip2.totalCost(rogue) + trip3.totalCost(rogue) + trip4.totalCost(rogue) + trip5.totalCost(rogue)
+puts corollaTripCost
+
+malibuTripCost = trip1.totalCost(malibu) + trip2.totalCost(malibu) + trip3.totalCost(malibu) + trip4.totalCost(malibu) + trip5.totalCost(malibu)
+puts corollaTripCost
+
+grandCherokeeTripCost = trip1.totalCost(grandCherokee) + trip2.totalCost(grandCherokee) + trip3.totalCost(grandCherokee) + trip4.totalCost(grandCherokee) + trip5.totalCost(grandCherokee)
+puts grandCherokeeTripCost
